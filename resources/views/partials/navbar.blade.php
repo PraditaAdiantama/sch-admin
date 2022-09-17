edfe97229569b3a3cf928c5893b05e49a592f19a
Lyod <mpaproject307@gmail.com>
2022-09-17 17:07:10 +0700

    new look when add new student

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,7 +5,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav  mb-2 mb-lg-0">
+        <ul class="navbar-nav px-3 mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link {{ ($title == 'Siswa' | $title == 'Detail' ? 'active' : '') }}" href="/students">Student</a>
           </li>
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -9,7 +9,89 @@
     @endif
     <div id="table-header">
         <h1 class="text-light m-0">Student</h1>
-        <a href="modif/create" class="btn btn-success"><i class="bi bi-plus-circle-fill p-1"></i> Add New Student</a>
+        <a class="btn btn-success" id="myInput" data-bs-toggle="modal" data-bs-target="#myModal"><i class="bi bi-plus-circle-fill p-1"></i> Add New Student</a>
+    </div>
+    <div>
+        <div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Create Student</h5>
+                </div>
+                <div class="modal-body">
+                    <form action="/modif/createData" method="POST">
+                        @csrf
+                        <div class="mb-3 mt-0">
+                            <label for="title" class="form-label">Nis</label>
+                            <input type="number" name="nis" class="form-control @error('nis') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            @error('nis')
+                                <div class="invalid-feedback" id="test">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                          </div>
+                        <div class="mb-3">
+                          <label for="title" class="form-label">Name</label>
+                          <input type="text" name="name" class="form-control  @error('name') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+                          @error('name')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Gender</label>
+                            <select class="form-select" name="gender" aria-label="Default select example">
+                                <option value="Men">Man</option>
+                                <option value="Women">Women</option>
+                              </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Address</label>
+                            <input type="text" name="address" class="form-control  @error('address') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            @error('address')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Birth Place</label>
+                            <input type="text" name="birth_place" class="form-control  @error('birth_place') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            @error('birth_place')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Birth Date</label>
+                            <input type="date" name="birth_date" class="form-control  @error('birth_date') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            @error('birth_date')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Class</label>
+                            <input type="text" name="class" class="form-control  @error('class') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            @error('class')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        
+                      </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                  <button type="submit" class="btn btn-success">Save</button>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
     <div class="tbl-container">
         <div class="row g-3">    
@@ -34,6 +116,7 @@
                         <td>{{ $student->class }}</td>
                         <td>
                             <a href="student/{{ $student->id }}" class="text-decoration-none text-light btn btn-primary">Detail</a>
+                            <a href="modif/delete/{{ $student->id }}" class="text-decoration-none text-light btn btn-danger">Delete</a>
                         </td>
                     </tr>
                 @endforeach
