509c4d7ffc4f2dc2949e2cfad6b0dffa5e761e18
unknown <aditadiantama@gmail.com>
2023-11-01 15:22:54 +0700

    feat: modal add major modal and modal trigger

--- resources/views/pages/majors/home.blade.php
+++ resources/views/pages/majors/home.blade.php
@@ -2,10 +2,14 @@
 
 @section('content')
     <x-navbar title="Jurusan">
-        <x-search-form action="" placeholder="Cari Jurusan" />
+        <x-search-form action="" name="" placeholder="Cari Jurusan" />
     </x-navbar>
     <div class="container-sch-admin mx-auto">
+        <x-majors.modal :majors="$major_options"/>
         <div class="bg-white shadow pt-4 px-3 pb-2 rounded">
+            <div class="d-flex justify-content-end mb-3">
+                <x-modal.trigger text="Tambah"/>
+            </div>
             <x-majors.table :majors="$majors" />
         </div>
     </div>
