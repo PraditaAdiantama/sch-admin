6ae3a033c4c2fe9a0ba39c5039293761280f0104
unknown <aditadiantama@gmail.com>
2023-11-01 15:47:14 +0700

    feat: add student filter by major table

---
+++ app/View/Components/Majors/Students/Table.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components\Majors\Students;
+
+use Illuminate\View\Component;
+
+class Table extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public $students
+    ) {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.majors.students.table');
+    }
+}
