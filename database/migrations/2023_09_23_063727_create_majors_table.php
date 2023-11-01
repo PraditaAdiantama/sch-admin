2f0f534cdd95b675d67dc9a6810cbde2de3de341
unknown <aditadiantama@gmail.com>
2023-11-01 15:45:12 +0700

    feat: add school_year, grade, major column

--- database/migrations/2023_09_23_063727_create_majors_table.php
+++ database/migrations/2023_09_23_063727_create_majors_table.php
@@ -16,6 +16,21 @@ return new class extends Migration
         Schema::create('majors', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->string("school_year");
+            $table->enum("grade", [
+                "X", "XI", "XII"
+            ]);
+            $table->enum("major", [
+                "Broadcast TV & Film",
+                "Desain Komunikasi Visual",
+                "Kimia Analisis",
+                "Teknik Instalasi Tenaga Listrik",
+                "Teknik Otomasi Industri",
+                "Teknik Jaringan Komputer dan Telekomunikasi",
+                "Teknik Otomotif",
+                "Teknik Las",
+                "Teknik Mesin",
+            ]);
             $table->timestamps();
         });
     }
