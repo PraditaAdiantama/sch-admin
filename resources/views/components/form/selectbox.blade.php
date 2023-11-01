73f587e4b4ed66aedba43c941bd04670a22669c3
unknown <aditadiantama@gmail.com>
2023-11-01 15:36:49 +0700

    feat: add class to label

--- resources/views/components/form/selectbox.blade.php
+++ resources/views/components/form/selectbox.blade.php
@@ -1,5 +1,5 @@
 <div class="mb-3">
-    <label for="{{ $name }}-select">{{ $label }}</label>
+    <label for="{{ $name }}-select" class="form-label">{{ $label }}</label>
     <select name="{{ $name }}" id="{{ $name }}-select" class="form-select">
         {{ $slot }}
     </select>
