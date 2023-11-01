6bc8406ce9edefd324bd15ee1f5390a1f6690a10
unknown <aditadiantama@gmail.com>
2023-11-01 15:39:01 +0700

    rafactor: change route classes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,13 +1,15 @@
 <header class="position-sticky shadow bg-white" style="height: 100vh;width: 20rem">
     <div style="max-width: 240px;" class="mx-auto">
-        <h3 class="fw-bold text-center pt-4">School Admin</h3>
+        <h3 class="fw-bold text-center pt-4">
+            <a href="/" class="nav-link">School Admin</a>
+        </h3>
         <hr>
         <nav>
             <ul class="navbar-nav fw-bold">
                 <li class="nav-item ps-4 {{ $title == 'siswa' ? 'bg-primary text-white rounded' : '' }}"><a
                         href="{{ route('students.index') }}" class="nav-link">Siswa</a></li>
                 <li class="nav-item ps-4 {{ $title == 'jurusan' ? 'bg-primary text-white rounded' : '' }}"><a
-                        href="{{ route('majors.index') }}" class="nav-link">Jurusan</a></li>
+                        href="{{ route('classes.index') }}" class="nav-link">Kelas</a></li>
                 <li class="nav-item ps-4 {{ $title == 'mapel' ? 'bg-primary text-white rounded' : '' }}"><a
                         href="" class="nav-link">Mata Pelajaran</a></li>
             </ul>
